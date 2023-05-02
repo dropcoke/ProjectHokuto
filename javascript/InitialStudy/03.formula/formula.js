c9c890f596ace8111a422c30998fb69d6b9fad77
dropcoke <shumatz@gmail.com>
2023-05-02 13:04:02 +0900

    Web画面で実行できるようにする。

---
+++ javascript/InitialStudy/03.formula/formula.js
@@ -0,0 +1,372 @@
+$(() => {
+    document.getElementById('btn-execute1').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program1 = document.getElementById("program-area1");
+        // メッセージ表示領域
+        const messageElement1 = document.getElementById('message1');
+        const errorHeader1 = document.getElementById('error-header1');
+        messageElement1.innerText = '';
+        errorHeader1.innerText = '';
+        try {
+            program1 && eval(program1.value);
+        }
+        catch (exception) {
+            messageElement1.innerText = exception.toString();
+            errorHeader1.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute2').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program2 = document.getElementById("program-area2");
+        // メッセージ表示領域
+        const messageElement2 = document.getElementById('message2');
+        const errorHeader2 = document.getElementById('error-header2');
+        messageElement2.innerText = '';
+        errorHeader2.innerText = '';
+        try {
+            program2 && eval(program2.value);
+        }
+        catch (exception) {
+            messageElement2.innerText = exception.toString();
+            errorHeader2.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute3').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program3 = document.getElementById("program-area3");
+        // メッセージ表示領域
+        const messageElement3 = document.getElementById('message3');
+        const errorHeader3 = document.getElementById('error-header3');
+        messageElement3.innerText = '';
+        errorHeader3.innerText = '';
+        try {
+            program3 && eval(program3.value);
+        }
+        catch (exception) {
+            messageElement3.innerText = exception.toString();
+            errorHeader3.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute4').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program4 = document.getElementById("program-area4");
+        // メッセージ表示領域
+        const messageElement4 = document.getElementById('message4');
+        const errorHeader4 = document.getElementById('error-header4');
+        messageElement4.innerText = '';
+        errorHeader4.innerText = '';
+        try {
+            program4 && eval(program4.value);
+        }
+        catch (exception) {
+            messageElement4.innerText = exception.toString();
+            errorHeader4.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute5').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program5 = document.getElementById("program-area5");
+        // メッセージ表示領域
+        const messageElement5 = document.getElementById('message5');
+        const errorHeader5 = document.getElementById('error-header5');
+        messageElement5.innerText = '';
+        errorHeader5.innerText = '';
+        try {
+            program5 && eval(program5.value);
+        }
+        catch (exception) {
+            messageElement5.innerText = exception.toString();
+            errorHeader5.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute6').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program6 = document.getElementById("program-area6");
+        // メッセージ表示領域
+        const messageElement6 = document.getElementById('message6');
+        const errorHeader6 = document.getElementById('error-header6');
+        messageElement6.innerText = '';
+        errorHeader6.innerText = '';
+        try {
+            program6 && eval(program6.value);
+        }
+        catch (exception) {
+            messageElement6.innerText = exception.toString();
+            errorHeader6.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute7').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program7 = document.getElementById("program-area7");
+        // メッセージ表示領域
+        const messageElement7 = document.getElementById('message7');
+        const errorHeader7 = document.getElementById('error-header7');
+        messageElement7.innerText = '';
+        errorHeader7.innerText = '';
+        try {
+            program7 && eval(program7.value);
+        }
+        catch (exception) {
+            messageElement7.innerText = exception.toString();
+            errorHeader7.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute8').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program8 = document.getElementById("program-area8");
+        // メッセージ表示領域
+        const messageElement8 = document.getElementById('message8');
+        const errorHeader8 = document.getElementById('error-header8');
+        messageElement8.innerText = '';
+        errorHeader8.innerText = '';
+        try {
+            program8 && eval(program8.value);
+        }
+        catch (exception) {
+            messageElement8.innerText = exception.toString();
+            errorHeader8.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute9').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program9 = document.getElementById("program-area9");
+        // メッセージ表示領域
+        const messageElement9 = document.getElementById('message9');
+        const errorHeader9 = document.getElementById('error-header9');
+        messageElement9.innerText = '';
+        errorHeader9.innerText = '';
+        try {
+            program9 && eval(program9.value);
+        }
+        catch (exception) {
+            messageElement9.innerText = exception.toString();
+            errorHeader9.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute10').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program10 = document.getElementById("program-area10");
+        // メッセージ表示領域
+        const messageElement10 = document.getElementById('message10');
+        const errorHeader10 = document.getElementById('error-header10');
+        messageElement10.innerText = '';
+        errorHeader10.innerText = '';
+        try {
+            program10 && eval(program10.value);
+        }
+        catch (exception) {
+            messageElement10.innerText = exception.toString();
+            errorHeader10.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute22').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program22 = document.getElementById("program-area22");
+        // メッセージ表示領域
+        const messageElement22 = document.getElementById('message22');
+        const errorHeader22 = document.getElementById('error-header22');
+        messageElement22.innerText = '';
+        errorHeader22.innerText = '';
+        try {
+            program22 && eval(program22.value);
+        }
+        catch (exception) {
+            messageElement22.innerText = exception.toString();
+            errorHeader22.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute11').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program11 = document.getElementById("program-area11");
+        // メッセージ表示領域
+        const messageElement11 = document.getElementById('message11');
+        const errorHeader11 = document.getElementById('error-header11');
+        messageElement11.innerText = '';
+        errorHeader11.innerText = '';
+        try {
+            program11 && eval(program11.value);
+        }
+        catch (exception) {
+            messageElement11.innerText = exception.toString();
+            errorHeader11.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute12').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program12 = document.getElementById("program-area12");
+        // メッセージ表示領域
+        const messageElement12 = document.getElementById('message12');
+        const errorHeader12 = document.getElementById('error-header12');
+        messageElement12.innerText = '';
+        errorHeader12.innerText = '';
+        try {
+            program12 && eval(program12.value);
+        }
+        catch (exception) {
+            messageElement12.innerText = exception.toString();
+            errorHeader12.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute13').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program13 = document.getElementById("program-area13");
+        // メッセージ表示領域
+        const messageElement13 = document.getElementById('message13');
+        const errorHeader13 = document.getElementById('error-header13');
+        messageElement13.innerText = '';
+        errorHeader13.innerText = '';
+        try {
+            program13 && eval(program13.value);
+        }
+        catch (exception) {
+            messageElement13.innerText = exception.toString();
+            errorHeader13.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute14').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program14 = document.getElementById("program-area14");
+        // メッセージ表示領域
+        const messageElement14 = document.getElementById('message14');
+        const errorHeader14 = document.getElementById('error-header14');
+        messageElement14.innerText = '';
+        errorHeader14.innerText = '';
+        try {
+            program14 && eval(program14.value);
+        }
+        catch (exception) {
+            messageElement14.innerText = exception.toString();
+            errorHeader14.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute15').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program15 = document.getElementById("program-area15");
+        // メッセージ表示領域
+        const messageElement15 = document.getElementById('message15');
+        const errorHeader15 = document.getElementById('error-header15');
+        messageElement15.innerText = '';
+        errorHeader15.innerText = '';
+        try {
+            program15 && eval(program15.value);
+        }
+        catch (exception) {
+            messageElement15.innerText = exception.toString();
+            errorHeader15.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute16').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program16 = document.getElementById("program-area16");
+        // メッセージ表示領域
+        const messageElement16 = document.getElementById('message16');
+        const errorHeader16 = document.getElementById('error-header16');
+        messageElement16.innerText = '';
+        errorHeader16.innerText = '';
+        try {
+            program16 && eval(program16.value);
+        }
+        catch (exception) {
+            messageElement16.innerText = exception.toString();
+            errorHeader16.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute17').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program17 = document.getElementById("program-area17");
+        // メッセージ表示領域
+        const messageElement17 = document.getElementById('message17');
+        const errorHeader17 = document.getElementById('error-header17');
+        messageElement17.innerText = '';
+        errorHeader17.innerText = '';
+        try {
+            program17 && eval(program17.value);
+        }
+        catch (exception) {
+            messageElement17.innerText = exception.toString();
+            errorHeader17.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute18').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program18 = document.getElementById("program-area18");
+        // メッセージ表示領域
+        const messageElement18 = document.getElementById('message18');
+        const errorHeader18 = document.getElementById('error-header18');
+        messageElement18.innerText = '';
+        errorHeader18.innerText = '';
+        try {
+            program18 && eval(program18.value);
+        }
+        catch (exception) {
+            messageElement18.innerText = exception.toString();
+            errorHeader18.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute19').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program19 = document.getElementById("program-area19");
+        // メッセージ表示領域
+        const messageElement19 = document.getElementById('message19');
+        const errorHeader19 = document.getElementById('error-header19');
+        messageElement19.innerText = '';
+        errorHeader19.innerText = '';
+        try {
+            program19 && eval(program19.value);
+        }
+        catch (exception) {
+            messageElement19.innerText = exception.toString();
+            errorHeader19.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute20').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program20 = document.getElementById("program-area20");
+        // メッセージ表示領域
+        const messageElement20 = document.getElementById('message20');
+        const errorHeader20 = document.getElementById('error-header20');
+        messageElement20.innerText = '';
+        errorHeader20.innerText = '';
+        try {
+            program20 && eval(program20.value);
+        }
+        catch (exception) {
+            messageElement20.innerText = exception.toString();
+            errorHeader20.innerText = 'エラーが発生しました。';
+        }
+    });
+    document.getElementById('btn-execute21').addEventListener('click', () => {
+        // プログラム編集用テキストエリア
+        let program21 = document.getElementById("program-area21");
+        // メッセージ表示領域
+        const messageElement21 = document.getElementById('message21');
+        const errorHeader21 = document.getElementById('error-header21');
+        messageElement21.innerText = '';
+        errorHeader21.innerText = '';
+        try {
+            program21 && eval(program21.value);
+        }
+        catch (exception) {
+            messageElement21.innerText = exception.toString();
+            errorHeader21.innerText = 'エラーが発生しました。';
+        }
+    });
+
+
+});
+// 最初の値
+const getFirstValue = () => {
+    return document.getElementById("first").value;
+}
+// 2番目の値
+const getSecondValue = () => {
+    return document.getElementById("second").value;
+}
+// 計算式
+const getFormula = () => {
+    return document.getElementById("formula").value;
+}
+const showResult = (value) => {
+    const target = document.getElementById("result");
+    target.value = value;
+}
