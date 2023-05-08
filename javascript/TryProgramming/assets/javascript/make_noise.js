3d519c28bfa732beeca64f7ff1db423dbe80319a
Shuichi Matsumoto <matsumoto@certio.co.jp>
2023-05-08 09:46:48 +0900

    キーボード部修正

--- javascript/TryProgramming/assets/javascript/make_noise.js
+++ javascript/TryProgramming/assets/javascript/make_noise.js
@@ -33,24 +33,16 @@ $(() => {
 const noteMap = [
     ["ド", "レ", "ミ", "ファ", "ソ", "ラ", "シ"],
     ["playC1();", "playD1();", "playE1();", "playF1();", "playG1();", "playA1();", "playB1();"],
+    ["onC1();", "onD1();", "onE1();", "onF1();", "onG1();", "onA1();", "onB1();"],
     ["ド+1", "レ+1", "ミ+1", "ファ+1", "ソ+1", "ラ+1", "シ+1"],
     ["playC2();", "playD2();", "playE2();", "playF2();", "playG2();", "playA2();", "playB2();"],
+    ["onC2();", "onD2();", "onE2();", "onF2();", "onG2();", "onA2();", "onB2();"],
     ["ド+2", "レ+2", "ミ+2", "ファ+2", "ソ+2", "ラ+2", "シ+2"],
     ["playC3();", "playD3();", "playE3();", "playF3();", "playG3();", "playA3();", "playB3();"],
+    ["onC3();", "onD3();", "onE3();", "onF3();", "onG3();", "onA3();", "onB3();"],
 ]
 
 const keyColor = ["steelblue", "aquamarine", "hotpink", "gold", "rosybrown", "darkviolet", "salmon"];
-const createTable = () => {
-    const tableStart = '<table id="block_table" class="table table-bordered" style="border-color: black;"><tbody>';
-    const tableEnd = '</tbody></table>';
-    let tdTag = '<td id="###,$$$" style="width: 75px; height: 75px; text-align: center; background: #000000" class="align-middle"></td>';
-    let tableContent = '';
-    for (let i = 0; i < tableCells.length; i++) {
-        for (let j = 0; i < tableCells[i].length; j++) {
-
-        }
-    }
-}
 
 
 let playMySynth = null;
@@ -97,60 +89,147 @@ const playB1 = () => {
 }
 const playC2 = () => {
     startSound(sound, 15, noteLength);
-    playMySynth.markNote(2, 0, noteLength);
+    playMySynth.markNote(3, 0, noteLength);
 }
 const playD2 = () => {
     startSound(sound, 16, noteLength);
-    playMySynth.markNote(2, 1, noteLength);
+    playMySynth.markNote(3, 1, noteLength);
 }
 const playE2 = () => {
     startSound(sound, 17, noteLength);
-    playMySynth.markNote(2, 2, noteLength);
+    playMySynth.markNote(3, 2, noteLength);
 }
 const playF2 = () => {
     startSound(sound, 18, noteLength);
-    playMySynth.markNote(2, 3, noteLength);
+    playMySynth.markNote(3, 3, noteLength);
 }
 const playG2 = () => {
     startSound(sound, 19, noteLength);
-    playMySynth.markNote(2, 4, noteLength);
+    playMySynth.markNote(3, 4, noteLength);
 }
 const playA2 = () => {
     startSound(sound, 20, noteLength);
-    playMySynth.markNote(2, 5, noteLength);
+    playMySynth.markNote(3, 5, noteLength);
 }
 const playB2 = () => {
     startSound(sound, 21, noteLength);
-    playMySynth.markNote(2, 6, noteLength);
+    playMySynth.markNote(3, 6, noteLength);
 }
 const playC3 = () => {
     startSound(sound, 22, noteLength);
-    playMySynth.markNote(4, 0, noteLength);
+    playMySynth.markNote(3, 0, noteLength);
 }
 const playD3 = () => {
     startSound(sound, 23, noteLength);
-    playMySynth.markNote(4, 1, noteLength);
+    playMySynth.markNote(6, 1, noteLength);
 }
 const playE3 = () => {
     startSound(sound, 24, noteLength);
-    playMySynth.markNote(4, 2, noteLength);
+    playMySynth.markNote(6, 2, noteLength);
 }
 const playF3 = () => {
     startSound(sound, 25, noteLength);
-    playMySynth.markNote(4, 3, noteLength);
+    playMySynth.markNote(6, 3, noteLength);
 }
 const playG3 = () => {
     startSound(sound, 26, noteLength);
-    playMySynth.markNote(4, 4, noteLength);
+    playMySynth.markNote(6, 4, noteLength);
 }
 const playA3 = () => {
     startSound(sound, 27, noteLength);
-    playMySynth.markNote(4, 5, noteLength);
+    playMySynth.markNote(6, 5, noteLength);
 }
 const playB3 = () => {
     startSound(sound, 28, noteLength);
-    playMySynth.markNote(4, 6, noteLength);
+    playMySynth.markNote(6, 6, noteLength);
 }
+
+// マウスクリック
+const onC1 = () => {
+    playMySynth.startSound(sound, 8);
+    playMySynth.markNote(0, 0);
+}
+const onD1 = () => {
+    playMySynth.startSound(sound, 9);
+    playMySynth.markNote(0, 1);
+}
+const onE1 = () => {
+    playMySynth.startSound(sound, 10);
+    playMySynth.markNote(0, 2);
+}
+const onF1 = () => {
+    playMySynth.startSound(sound, 11);
+    playMySynth.markNote(0, 3);
+}
+const onG1 = () => {
+    playMySynth.startSound(sound, 12);
+    playMySynth.markNote(0, 4);
+}
+const onA1 = () => {
+    playMySynth.startSound(sound, 13);
+    playMySynth.markNote(0, 5);
+}
+const onB1 = () => {
+    playMySynth.startSound(sound, 14);
+    playMySynth.markNote(0, 6);
+}
+const onC2 = () => {
+    playMySynth.startSound(sound, 15);
+    playMySynth.markNote(3, 0);
+}
+const onD2 = () => {
+    playMySynth.startSound(sound, 16);
+    playMySynth.markNote(3, 1);
+}
+const onE2 = () => {
+    playMySynth.startSound(sound, 17);
+    playMySynth.markNote(3, 2);
+}
+const onF2 = () => {
+    playMySynth.startSound(sound, 18);
+    playMySynth.markNote(3, 3);
+}
+const onG2 = () => {
+    playMySynth.startSound(sound, 19);
+    playMySynth.markNote(3, 4);
+}
+const onA2 = () => {
+    playMySynth.startSound(sound, 20);
+    playMySynth.markNote(3, 5);
+}
+const onB2 = () => {
+    playMySynth.startSound(sound, 21);
+    playMySynth.markNote(3, 6);
+}
+const onC3 = () => {
+    playMySynth.startSound(sound, 22);
+    playMySynth.markNote(3, 0);
+}
+const onD3 = () => {
+    playMySynth.startSound(sound, 23);
+    playMySynth.markNote(6, 1);
+}
+const onE3 = () => {
+    playMySynth.startSound(sound, 24);
+    playMySynth.markNote(6, 2);
+}
+const onF3 = () => {
+    playMySynth.startSound(sound, 25);
+    playMySynth.markNote(6, 3);
+}
+const onG3 = () => {
+    playMySynth.startSound(sound, 26);
+    playMySynth.markNote(6, 4);
+}
+const onA3 = () => {
+    playMySynth.startSound(sound, 27);
+    playMySynth.markNote(6, 5);
+}
+const onB3 = () => {
+    playMySynth.startSound(sound, 28);
+    playMySynth.markNote(6, 6);
+}
+
 const PlayMySynth = {
     outputArea: null,
     tableBlock: [],
@@ -158,7 +237,7 @@ const PlayMySynth = {
     keyOn: false,
     noteWaiting: [],
     currentNoteLength: 0,
-    getCellElemet(y, x) {
+    getCellElement(y, x) {
         return document.getElementById(y + "," + x);
     },
     currentNotCell: {y: 0, x: 0},
@@ -169,11 +248,11 @@ const PlayMySynth = {
             return;
         }
         this.keyOn = true;
-        this.getCellElemet(y, x).style.background = keyColor[x];
+        this.getCellElement(y, x).style.background = keyColor[x];
         this.currentNotCell = {y: y, x: x};
-        setTimeout(() => {
-            this.getCellElemet(this.currentNotCell.y, this.currentNotCell.x).style.background = 'white';
-            this.keyOn = false;}, length * 1000);
+        // setTimeout(() => {
+        //     this.getCellElement(this.currentNotCell.y, this.currentNotCell.x).style.background = 'white';
+        //     this.keyOn = false;}, length * 1000);
         this.currentNoteLength = length * 1000;
     },
     drawTable() {
@@ -204,7 +283,10 @@ const PlayMySynth = {
             this.cells[i] = []
                 for (let j = 0; j < x; j++) {
                     this.cells[i][j] = document.getElementById(this.tableBlock[i][j]);
-                    this.cells[i][j].addEventListener('click', (e) => {
+                    if (i % 3 === 2) {
+                        this.cells[i][j].style.display = "none";
+                    }
+                    this.cells[i][j].addEventListener('mousedown', (e) => {
                         let target = e.target;
                         if(!e.target.id) {
                             target = e.target.parentElement;
@@ -212,14 +294,19 @@ const PlayMySynth = {
                         const position = target.id.split(",");
                         const y = Number(position[0]);
                         const x = Number(position[1]);
-                        if (y % 2 != 0) {
+                        if (y % 3 != 0) {
                             eval(target.innerText)
                         }
                         else {
-                            const playCellId = [y + 1, x];
+                            const playCellId = [y + 2, x];
                             const playCell = document.getElementById(playCellId.join(','));
                             eval(playCell.innerText);
                         }
+
+                    });
+                    this.cells[i][j].addEventListener('mouseup', (e) => {
+                        // alert();
+                        playMySynth.stopSound();
 
                     });
                 }
@@ -229,41 +316,37 @@ const PlayMySynth = {
     playing: false,
     waiting: [],
     currentLength: 0,
-    playSound(oscillator, note = 0, length = 1) {
-        const volume = 1;
-        const oscillatorTypes = ['sine', 'square', 'sawtooth', 'triangle'];
-        // 音階を周波数に設定
-        notes = {'C1': 65.4,
-        'D1': 73.41,
-        'E1': 82.4,
-        'F1': 87.3,
-        'G1': 97.99,
-        'A1': 110,
-        'B1':123.47,
-        'C2':130.81,
-        'D2':146.83,
-        'E2':164.81,
-        'F2':174.61,
-        'G2':195.99,
-        'A2':220,
-        'B2':246.94,
-        'C3':261.62,
-        'D3':293.66,
-        'E3':329.62,
-        'F3':349.22,
-        'G3':391.99,
-        'A3':440,
-        'B3':493.88,
-        'C4':523.25,
-        'D4':587.33,
-        'E4':659.25,
-        'F4':698.45,
-        'G4':783.99,
-        'A4':880,
-        'B4':987.76,
-        };
-        // 音階
-        keys = ['C1',
+    oscillatorTypes:  ['sine', 'square', 'sawtooth', 'triangle'],
+    notes:  {'C1': 65.4,
+            'D1': 73.41,
+            'E1': 82.4,
+            'F1': 87.3,
+            'G1': 97.99,
+            'A1': 110,
+            'B1':123.47,
+            'C2':130.81,
+            'D2':146.83,
+            'E2':164.81,
+            'F2':174.61,
+            'G2':195.99,
+            'A2':220,
+            'B2':246.94,
+            'C3':261.62,
+            'D3':293.66,
+            'E3':329.62,
+            'F3':349.22,
+            'G3':391.99,
+            'A3':440,
+            'B3':493.88,
+            'C4':523.25,
+            'D4':587.33,
+            'E4':659.25,
+            'F4':698.45,
+            'G4':783.99,
+            'A4':880,
+            'B4':987.76,
+            },
+    keys: ['C1',
             'D1',
             'E1',
             'F1',
@@ -291,11 +374,13 @@ const PlayMySynth = {
             'G4',
             'A4',
             'B4',
-            ];
-        if (note <= 0 || note > keys.length){
+            ],
+    playSound(oscillator, note = 0, length = 1) {
+        const volume = 1;
+        if (note <= 0 || note > this.keys.length){
             throw new Error('その音程はありません' + note);
         }
-        if (oscillator <= 0 || oscillator > oscillatorTypes.length){
+        if (oscillator <= 0 || oscillator > this.oscillatorTypes.length){
             throw new Error('その音程はありません');
         }
         if (this.playing) {
@@ -306,16 +391,22 @@ const PlayMySynth = {
             // console.log(keys[note - 1]);
             // console.log(oscillatorTypes[oscillator - 1]);
             // console.log(this.currentLength);
-            this.synth.play(-10, notes[keys[note - 1]], oscillatorTypes[oscillator - 1]);
+            this.synth.play(-10, this.notes[this.keys[note - 1]], this.oscillatorTypes[oscillator - 1]);
             setTimeout(() => {this.stopSound();}, length * 1000);
             this.currentLength = length * 1000;
             this.playing = true;
         }
     },
+    startSound(oscillator, note = 0) {
+        this.synth.play(-10, this.notes[this.keys[note - 1]], this.oscillatorTypes[oscillator - 1]);
+        this.playing = true;
+    },
     stopSound() {
         // console.log("stop")
         this.synth.stop();
         this.playing = false;
+        this.getCellElement(this.currentNotCell.y, this.currentNotCell.x).style.background = 'white';
+        this.keyOn = false;
     }
 }
 
